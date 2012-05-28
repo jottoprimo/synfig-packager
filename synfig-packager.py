8a0042b3c93971d3d7991feb9cb2de5eb60df94a
Евгений Катунов <jottoprimo@gmail.com>
2012-05-28 17:58:14 +0700

    исправлен баг с различной работой getcwd(python) и pwd(bash) в отношении ссылок

--- synfig-packager.py
+++ synfig-packager.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys, os, shutil, re, zipfile, random, gzip, tempfile
+import subprocess
 
 global parslist
 
@@ -17,6 +18,11 @@ zip_dirs=[]
 lst_image_file=''
 #parslist2=[]
 
+def myabspath(path):
+	proc = subprocess.Popen('pwd', shell = True, stdout=subprocess.PIPE)
+	pwd = proc.stdout.read()
+	return os.path.normpath(os.path.join(pwd, path))
+
 def _callback(matches):
     id = matches.group(1)
     try:
@@ -27,62 +33,6 @@ def _callback(matches):
 def decode_unicode_references(data):
     return re.sub("&#x([a-zA-Z0-9]+)(;|(?=\s))", _callback, data)
 
-def sifparse(filename):
-	#print "Parsing file %s..." % (filename)
-	sifdir=filename[:filename.find(os.path.basename(filename))]
-	file=open(filename)
-	parslist.append(filename)
-	massiv=file.readlines()
-	for i, line in enumerate(massiv):
-		if line.find('<param name="filename">')<>-1:
-			str=massiv[i+1]
-			pos1=str.find('<string>')+len('<string>')
-			pos2=str.find('</string>')
-			fname=str[pos1:pos2]
-			#print "       Join input:",sifdir,fn 
-			fn=os.path.join(sifdir ,fname)
-			fn=os.path.abspath(fn)
-			if not fn in filelist:
-				print fn
-				fname=os.path.basename(fname)
-				co=a1+'/'+fname
-				shutil.copy(fn, co)
-			filelist.append(fn)
-		if line.find('<param name="canvas" use=')<>-1:
-			pos1=line.find('<param name="canvas" use="')+len('<param name="canvas" use="')
-			pos2=line.find('#"/>')
-			fname=line[pos1:pos2]
-			fn=os.path.abspath(fname)
-			print fn
-			if not fn in parslist:
-				fname=os.path.basename(fname)
-				co=a1+'/'+fname
-				shutil.copy(fn, co)
-				filedit(co)
-				sifparse(fn)
-
-def filedit(cname):
-	file2=open(cname)
-	parslist2.append(cname)
-	massiv2=file2.readlines()
-	for j, line2 in file2.readline():
-		if line2.find('<param name="filename">')<>-1:
-			str2=massiv2[j+1]
-			pos1=str2.find('<string>')+len('<string>')
-			pos2=str2.find('</string>')
-			fname2=str2[pos1:pos2] 
-			fname2=os.path.basename(fname2)
-			massiv2[j+1]='<string>'+fname2+'</string>'
-		if line2.find('<param name="canvas" use=')<>-1:
-			pos1=line2.find('<param name="canvas" use="')+len('<param name="canvas" use="')
-			pos2=line2.find('#"/>')
-			fname2=line2[pos1:pos2]
-			fname2=os.path.basename(fname2)
-			file2.write='<param name="canvas" use='+fname2+'#"/>'
-			file2.close()
-			if not fname2 in parslist2:
-				filedit(fname2)
-	file2.close()
 
 def copy_image(inputt, outputt, t):
 	#print "=== Copy image called! ==="
@@ -307,7 +257,7 @@ while len(unparsed)>0:
 			#print "       Join input:",sifdir,fn 
 			fn=os.path.join(sifdir ,fnamenotsif)
 			#print "+++",sifdir,"___",fnamenotsif
-			fn=os.path.abspath(fn)
+			fn=myabspath(fn)
 			if fn.find('&#x')<>-1:
 				#decoder=fn[fn.find('&#x')+3:fn.find(';')]
 				#print decoder
@@ -372,7 +322,7 @@ while len(unparsed)>0:
 			#print "       Join input:",sifdir,fn 
 			fn=os.path.join(sifdir ,font_name)
 			#print "+++",sifdir,"___",fnamenotsif
-			fn=os.path.abspath(fn)
+			fn=myabspath(fn)
 			if fn.find('&#x')<>-1:
 				fn=decode_unicode_references(fn)
 				fn=fn.encode('utf-8')
